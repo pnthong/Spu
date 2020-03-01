1b3294bbe7054cae7f75af5ab45fbf4ccb5a0a2f
hmphuoc <minhphuochong1985@gmail.com>
2020-03-01 19:17:43 +0900

    Use the variadic template instead of the variadic macro

--- Spu/src/Spu/Logger.h
+++ Spu/src/Spu/Logger.h
@@ -23,16 +23,54 @@ namespace Spu {
 	};
 }
 
-// log macros for the engine
-#define LogCoreTrace(...)	Spu::Logger::GetCoreLogger()->trace(__VA_ARGS__)
-#define LogCoreInfo(...)	Spu::Logger::GetCoreLogger()->info(__VA_ARGS__)
-#define LogCoreWarn(...)	Spu::Logger::GetCoreLogger()->warn(__VA_ARGS__)
-#define LogCoreError(...)	Spu::Logger::GetCoreLogger()->error(__VA_ARGS__)
-#define LogCoreFatal(...)	Spu::Logger::GetCoreLogger()->fatal(__VA_ARGS__)
+// for the engine
+template<typename ...Args>
+inline void LogCoreTrace(const char* format, Args...args) {
+	Spu::Logger::GetCoreLogger()->trace(format, std::forward<Args>(args)...);
+}
 
-// log macros for clients
-#define LogTrace(...)	Spu::Logger::GetClientLogger()->trace(__VA_ARGS__)
-#define LogInfo(...)	Spu::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define LogWarn(...)	Spu::Logger::GetClientLogger()->warn(__VA_ARGS__)
-#define LogError(...)	Spu::Logger::GetClientLogger()->error(__VA_ARGS__)
-#define LogFatal(...)	Spu::Logger::GetClientLogger()->fatal(__VA_ARGS__)
+template<typename ...Args>
+inline void LogCoreInfo(const char* format, Args...args) {
+	Spu::Logger::GetCoreLogger()->info(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogCoreWarn(const char* format, Args...args) {
+	Spu::Logger::GetCoreLogger()->warn(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogCoreError(const char* format, Args...args) {
+	Spu::Logger::GetCoreLogger()->error(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogCoreFatal(const char* format, Args...args) {
+	Spu::Logger::GetCoreLogger()->fatal(format, std::forward<Args>(args)...);
+}
+
+// For clients
+template<typename ...Args>
+inline void LogTrace(const char* format, Args...args) {
+	Spu::Logger::GetClientLogger()->trace(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogInfo(const char* format, Args...args) {
+	Spu::Logger::GetClientLogger()->info(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogWarn(const char* format, Args...args) {
+	Spu::Logger::GetClientLogger()->warn(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogError(const char* format, Args...args) {
+	Spu::Logger::GetClientLogger()->error(format, std::forward<Args>(args)...);
+}
+
+template<typename ...Args>
+inline void LogFatal(const char* format, Args...args) {
+	Spu::Logger::GetClientLogger()->fatal(format, std::forward<Args>(args)...);
+}
